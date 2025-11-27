9ca6d80b96b12443abf6216a48f387e45a77b6d4
anshulyadav1791 <anshulyadav1791@gmail.com>
2025-11-27 12:01:33 +0530

    add new file

--- FullApp/backend/courses/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .serializers import CourseSerializer
-from .models import Course
-
-class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
--- FullApp/backend/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from main import views
-
-
-urlpatterns = [
-    path('', views.home , name="home"),
-    path('about', views.about , name="about"),
-    path('contact', views.contact , name="contact"),
-]
