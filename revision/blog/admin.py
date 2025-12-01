65ea3b421210e85a656c784aed99cea3e90d4c3b
anshulyadav1791 <anshulyadav1791@gmail.com>
2025-12-01 15:22:33 +0530

    add new

--- revision/blog/admin.py
+++ revision/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import m_ki_c_Admin
 
-# Register your models here.
+@admin.register(m_ki_c_Admin)
+class StudentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'age','age', )
--- revision/blog/models.py
+++ revision/blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Student(models.Model):
+class m_ki_c_Admin(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField(unique=True)
     enrollment_data = models.DateField(auto_now_add=True)
-    # city = models.CharField(max_length=100,default='Unknown')
+    # city = models.CharField(max_length=100,d2efault='Unknown')
 
     def __str__(self):
         return self.name
