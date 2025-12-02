e070278c3938c946b88c5f7eca20e0c8072acafb
anshulyadav1791 <anshulyadav1791@gmail.com>
2025-12-02 17:09:48 +0530

    newfile

--- revision/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import m_ki_c_Admin
-
-@admin.register(m_ki_c_Admin)
-class StudentAdmin(admin.ModelAdmin):
-
-    list_display = ('name', 'age','age', )
--- revision/revision/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('blog.urls')), 
-    path('managedata/', admin.site.urls),
-
-]
